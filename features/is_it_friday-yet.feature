52e650802f3a33e254d0f266e85b3ca311655743
Nyayic Fanny <chrosbiefannie@gmail.com>
2020-03-05 10:25:19 +0300

    Writing gherkin with cucumber

--- features/is_it_friday-yet.feature
+++ features/is_it_friday-yet.feature
@@ -3,6 +3,6 @@ Feature: Is it Friday yet?
    Everybody wants to know when it's Friday
 
    Scenario: Sunday isn't Friday
-   Given Today is Sunday
-   When I ask whether it's Friday yet
-   Then I should be told "nope" 
+     Given Today is Sunday
+     When I ask whether it's Friday yet
+     Then I should be told "Nope" 
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,5 +1,6 @@
 const assert = require('assert')
 const {Given, When, Then} = require('cucumber');
+
 function IsItFriday(today){
     if(today === "Friday"){
         return "TGIF"
@@ -8,3 +9,15 @@ function IsItFriday(today){
         return "Nope"
     }
 }
+
+Given ('Today is Sunday', function(){
+    this.today = 'Sunday';
+});
+
+When ('I ask whether it\'s Friday yet', function(){
+    this.actualAnswer = IsItFriday(this.today)
+});
+
+Then ("I should be told {string}", function(expectedAnswer){
+    assert.equal(this.actualAnswer, expectedAnswer);
+})
